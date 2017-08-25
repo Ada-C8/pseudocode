625c032961a00bbdf3fd05cbd373b6480f777ab2
dee <30602862+tildeee@users.noreply.github.com>
2017-08-24 22:26:57 -0700

    Update pseudocode.rb

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,7 +1,7 @@
 ##################
 ### OBJECTIVE:  ##
 ##################
-# Write a Deaf Grandma program. Whatever you say to grandma (whatever you type in), she should respond with  HUH?!  SPEAK UP, SONNY!, unless you shout it (type in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back, NO, NOT SINCE 1938!
+# Write a Dear Grandma program. Whatever you say to grandma (whatever you type in), she should respond with  HUH?!  SPEAK UP, SONNY!, unless you shout it (type in all capitals). If you shout, she can hear you (or at least she thinks so) and yells back, NO, NOT SINCE 1938!
 
 #INPUT:
   #A string of a sentence we are saying to grandma
